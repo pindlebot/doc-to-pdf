d19da80a4d6ae97d24023026d810e0c741f5e04e
Ben <menubar@protonmail.com>
2018-07-24 17:16:07 -0400

    new

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-require('node-fetch')
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
-const Consumer = require('sqs-consumer')
 const { exec } = require('child_process')
 const { loadEnv } = require('parameter-store')
 const { PassThrough } = require('stream')
@@ -15,12 +13,10 @@ const {
 } = process.env
 process.env.AWS_BUCKET = 'printawesome'
 
-const LIBRE_OFFICE_TMP_DIR = process.env.LIBRE_OFFICE_TMP_DIR || '/var/app/current'
-const LIBRE_OFFICE_VERSION = process.env.LIBRE_OFFICE_VERSION || '6.0'
-// const LIBRE_OFFICE_TMP_DIR = '/Users/ben/repos/doc-to-pdf'
+const LIBRE_OFFICE_TMP_DIR = process.env.LIBRE_OFFICE_TMP_DIR || __dirname
 
 const command = filename =>
-  `sudo /opt/libreoffice${LIBRE_OFFICE_VERSION}/program/soffice --headless --convert-to pdf:writer_pdf_Export "${filename}" --outdir ${LIBRE_OFFICE_TMP_DIR}`
+  `sudo /opt/libreoffice*/program/soffice --headless --convert-to pdf:writer_pdf_Export "${filename}" --outdir ${LIBRE_OFFICE_TMP_DIR}`
 
 const convert = async ({ key }, tmp = LIBRE_OFFICE_TMP_DIR) => {
   let [id, name] = key.split('/')
@@ -34,7 +30,7 @@ const convert = async ({ key }, tmp = LIBRE_OFFICE_TMP_DIR) => {
     .promise()
     .then(() => s3.getObject(params).createReadStream())
     .catch(err => {
-      console.log(err)
+      throw err
     })
   const documentPath = path.join(tmp, name)
   const pdfPath = path.join(tmp, `${basename}.pdf`)
@@ -46,13 +42,11 @@ const convert = async ({ key }, tmp = LIBRE_OFFICE_TMP_DIR) => {
   })
   await new Promise((resolve, reject) => {
     exec(command(documentPath), (err, stdout, stderr) => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        resolve(true)
-      }
+      if (err) reject(err)
+      else resolve(true)
     })
+  }).catch(err => {
+    throw err
   })
   let pass = new PassThrough()
   AWS.config.update({ region: AWS_REGION })
@@ -65,10 +59,7 @@ const convert = async ({ key }, tmp = LIBRE_OFFICE_TMP_DIR) => {
   })
   managedUpload.send()
   let readStream = fs.createReadStream(pdfPath)
-  readStream.on('error', console.log)
-  readStream.on('end', () => {
-    console.log('end')
-  })
+  readStream.on('error', console.error.bind(console))
   readStream.pipe(pass)
   await managedUpload.promise()
     .catch(err => console.log(err))
@@ -81,38 +72,21 @@ const convert = async ({ key }, tmp = LIBRE_OFFICE_TMP_DIR) => {
 async function init () {
   await loadEnv('/doc-to-pdf', { region: AWS_REGION })
 
-  let consumer = Consumer.create({
-    queueUrl: process.env.SQS_ENDPOINT,
-    messageAttributeNames: ['All'],
-    batchSize: 1,
-    handleMessage: (message, done) => {
-      let { MessageAttributes } = message
-      let params = { key: message.Body }
-      if (MessageAttributes.token) {
-        params.token = MessageAttributes.token.StringValue
-      }
-      convert(params).then(done)
-    }
-  })
-
-  consumer.start()
-
   http.createServer(async (req, res) => {
     let url = require('url').parse(req.url)
     let { pathname } = url
+    let key = decodeURIComponent(path.basename(pathname))
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.statusCode = 200
+
     if (pathname === '/') {
       res.setHeader('Content-Type', 'text/html')
       res.end('OK')
     } else if (pathname.startsWith('/convert')) {
-      let key = decodeURIComponent(path.basename(pathname))
-      console.log({ key })
       convert({ key })
       res.setHeader('Content-type', 'application/json')
       res.end(JSON.stringify({}))
     } else if (pathname.startsWith('/webhook')) {
-      let key = decodeURIComponent(path.basename(pathname))
       // let data = await pdf.createMessage({ messageType: 's3' }, key)
       res.setHeader('Content-type', 'application/json')
       res.end(JSON.stringify({}))
